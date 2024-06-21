0f225f38e2dfe0af2ea645076dd7ea8dbb621d6a
Fabian Michael <fabian.michael@reinorange.com>
2024-06-21 12:12:54 +0200

    fix: don’t break kirby’s builtin link field
    - resolve a naming collision between the preview component of both link fields

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
-import App from "./App.vue";
-import Preview from "./Preview.vue";
+import App from './App.vue'
+import Preview from './Preview.vue'
 
-panel.plugin("oblik/link-field", {
-	fields: {
-		linkfield: App,
-	},
-	components: {
-		"k-link-field-preview": Preview,
-	},
-});
+panel.plugin('oblik/link-field', {
+  fields: {
+    linkfield: App
+  },
+  components: {
+    'k-linkfield-field-preview': Preview
+  }
+})
